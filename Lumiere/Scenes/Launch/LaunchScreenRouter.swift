c69498bc2197eddc2c2c18f50769718b8fa6825c
pnalvarez <pedronalvarez@hotmail.com>
2020-07-02 20:25:57 -0300

    setup launch screen

---
+++ Lumiere/Scenes/Launch/LaunchScreenRouter.swift
@@ -0,0 +1,33 @@
+//
+//  LaunchScreenCoordinator.swift
+//  Lumiere
+//
+//  Created by Pedro Alvarez on 01/07/20.
+//  Copyright © 2020 Pedro Alvarez. All rights reserved.
+//
+
+import UIKit
+
+protocol LaunchScreenRouterProtocol: BaseRouterProtocol {
+    var window: UIWindow? { get set }
+    func routeToLoginScreen()
+}
+
+class LaunchScreenRouter: BaseRouterProtocol {
+    
+    weak var viewController: UIViewController?
+    var window: UIWindow?
+    
+    func routeTo(nextVC: UIViewController) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.window?.rootViewController = nextVC
+        }
+    }
+}
+
+extension LaunchScreenRouter: LaunchScreenRouterProtocol {
+    
+    func routeToLoginScreen() {
+        routeTo(nextVC: UIViewController())
+    }
+}
