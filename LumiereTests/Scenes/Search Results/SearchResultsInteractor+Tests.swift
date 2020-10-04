463d40dd16e9542233ca199af7b9a5723ebdd7f0
pnalvarez <pedronalvarez@hotmail.com>
2020-10-04 15:21:44 -0300

    finished testing search results interactor

--- LumiereTests/Scenes/Search Results/SearchResultsInteractor+Tests.swift
+++ LumiereTests/Scenes/Search Results/SearchResultsInteractor+Tests.swift
@@ -18,6 +18,7 @@ class SearchResultsInteractor_Tests: XCTestCase {
     var results: SearchResults.Info.Model.Results?
     var presentProfileDetailsFlag = false
     var presentProjectDetailsFlag = false
+    var error: SearchResults.Info.Model.ResultError?
 
     override func setUp() {
         super.setUp()
@@ -43,13 +44,29 @@ class SearchResultsInteractor_Tests: XCTestCase {
         XCTAssertEqual(expectedResult, results)
     }
     
+    func testFetchBeginSearch_Error() {
+        XCTAssertNil(error)
+        sut.searchKey = SearchResults.Info.Received.SearchKey(key: "ERROR")
+        sut.fetchBeginSearch(SearchResults.Request.Search())
+        XCTAssertNotNil(error)
+    }
+    
     func testFetchSearch_Success() {
+        XCTAssertNil(results)
         sut.fetchSearch(SearchResults.Request.SearchWithPreffix(preffix: "Test"))
         let expectedResult = SearchResults.Info.Model.Results(users: [SearchResults.Info.Model.Profile(id: "idUser1", name: "Usuario Teste 1", image: "image", ocupation: "Artist"), SearchResults.Info.Model.Profile(id: "idUser2", name: "Usuario Teste 2", image: "image", ocupation: "Artist"), SearchResults.Info.Model.Profile(id: "idUser3", name: "Usuario Teste 3", image: "image", ocupation: "Artist")], projects: [SearchResults.Info.Model.Project(id: "idProj1", title: "Projeto Teste 1", progress: 50, firstCathegory: "Ação", secondCathegory: "Animação", image: "image"), SearchResults.Info.Model.Project(id: "idProj2", title: "Projeto Teste 2", progress: 70, firstCathegory: "Ação", secondCathegory: "Animação", image: "image"), SearchResults.Info.Model.Project(id: "idProj3", title: "Projeto Teste 3", progress: 50, firstCathegory: "Ação", secondCathegory: "Animação", image: "image")])
         XCTAssertEqual(expectedResult, results)
+        XCTAssertNil(error)
+    }
+    
+    func testFetchSearch_Error() {
+        XCTAssertNil(error)
+        sut.fetchSearch(SearchResults.Request.SearchWithPreffix(preffix: "ERROR"))
+        XCTAssertNotNil(error)
     }
     
     func testFetchSelectProfile() {
+        XCTAssertNil(results)
         sut.results = SearchResults.Info.Model.Results.stub
         XCTAssertNil(sut.selectedItem)
         XCTAssertFalse(presentProjectDetailsFlag)
@@ -62,6 +79,7 @@ class SearchResultsInteractor_Tests: XCTestCase {
                                                                                image: "image",
                                                                                ocupation: "Artist")))
         XCTAssertTrue(presentProfileDetailsFlag)
+        XCTAssertNil(error)
     }
     
     func testFetchSelectProject() {
@@ -93,5 +111,9 @@ extension SearchResultsInteractor_Tests: SearchResultsPresentationLogic {
     
     func presentProjectDetails() {
         self.presentProjectDetailsFlag = true
+    }
+    
+    func presentError(_ response: SearchResults.Info.Model.ResultError) {
+        self.error = response
     }
 }
